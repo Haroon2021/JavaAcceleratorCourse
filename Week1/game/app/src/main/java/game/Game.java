d4a5d711c44c8983579f744c98ae5f39ad53f21e
Makers Admin <MakersAdmin@MacBook-Air.local>
2022-07-22 12:15:21 +0100

    Word Game Part 3 part way through

--- Week1/game/app/src/main/java/game/Game.java
+++ Week1/game/app/src/main/java/game/Game.java
@@ -1,35 +1,24 @@
 package game;
 
-import java.util.Random;
+import java.util.ArrayList;
+// import java.util.Random;
 
 public class Game {
 
     
     public Integer remainingAttempts = 10;
-   
-    public Game(String word) {
-        WordChooser wordchooser = new WordChooser();
-        word = wordchooser.getRandomWordFromDictionary();
+    private String word;
+    private ArrayList<Character> lettersGuessed = new ArrayList<Character>();
+
+    public Game(WordChooser wordchooser) {
+        this.word = wordchooser.getRandomWordFromDictionary();
       }
 
 
-    // public String GetWordToGuess() {
-
-    // StringBuilder hiddenWord = new StringBuilder(theword);
-
-    // for(int i = 0; i < hiddenWord.length(); i++) {
-    //     if (i != 0) {
-    //       hiddenWord.replace(i, i + 1, "_");
-    //     }
-    //   }
-    //   return hiddenWord.toString();
-    // }
-    // }
-
     public String getWordToGuess() {
       StringBuilder builder = new StringBuilder();
-      for (int i = 0; i < this.theword.length(); i++) {
-          Character currentLetter = theword.charAt(0);
+      for (int i = 0; i < this.word.length(); i++) {
+          Character currentLetter = word.charAt(0);
           if (i == 0) {
               builder.append(currentLetter);
           } else {
@@ -41,6 +30,16 @@ public class Game {
 
     public Integer getRemainingAttempts() {
       return this.remainingAttempts;
+    }
+
+    public Boolean guessLetter(Character guessLetter) {
+      if (this.word.indexOf(guessLetter) != -1) {
+        this.lettersGuessed.add(guessLetter);
+        return true;
+      } else {
+        this.remainingAttempts --;
+        return false;
+      }
     }
 
    
--- Week1/game/app/src/test/java/game/GameTest.java
+++ Week1/game/app/src/test/java/game/GameTest.java
@@ -1,22 +1,39 @@
 package game;
-
-import static org.junit.Assert.assertEquals;
-
+import static org.junit.Assert.*; // imports assertEquals, asserttrue assertfalse ect
+// import static org.junit.Assert.assertFalse;
+// import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.*;
-
+import org.junit.Before;
 import org.junit.Test;
 
 public class GameTest {
+    // the game created below allows us to use this.game later on (initialised var)
+    private Game game;
+    @Before
+    // This is done before each test
+    public void initialiseTests() {
+        // creating a mock wordchooser using its class
+        WordChooser mockWordChooser = mock(WordChooser.class);
+        // stubbing appears before the actual execution
+        // the order of the 2 lines below is important the function calls need to be specified before using the mock
+        when(mockWordChooser.getRandomWordFromDictionary()).thenReturn("MAKERS");
+        this.game = new Game(mockWordChooser);
+    }
     @Test 
     public void testGetsWordToGuess() {
-        Game game = new Game("MAKERS");
-        assertEquals(game.getWordToGuess(), "M_____");
-        // assertEquals(game.GetWordToGuess(),String.valueOf("M_____"));
+        assertEquals(this.game.getWordToGuess(), "M_____");
     }
     @Test
     public void testGetintialRemainingAttempts() {
-        Game game = new Game("MAKERS");
-        assertEquals(game.getRemainingAttempts(), Integer.valueOf(10));
+        assertEquals(this.game.getRemainingAttempts(), Integer.valueOf(10));
+    }
+    @Test
+    public void testGuessLetterReturnsTrueIfCorrectLetterGiven() {
+        assertTrue("Test passes if correct letter given",this.game.guessLetter('E'));
+    }
+    @Test
+    public void testGuessLetterReturnsFalseIfINorrectLetterGiven() {
+        assertFalse("Test fails if incorrect letter is given", this.game.guessLetter('Z'));
     }
 
 }
