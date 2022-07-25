2721459f7fdfd23490ffb2adada7826ef39dfffa
dhopz <david_houpapa@hotmail.com>
2022-07-25 11:50:30 +0100

    CLI added, working on Game Logic messages

--- Week1/game/app/src/main/java/game/App.java
+++ Week1/game/app/src/main/java/game/App.java
@@ -3,12 +3,28 @@
  */
 package game;
 
+import java.util.Scanner;
+
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return "Welcome! Today the word to guess is:";
     }
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+        WordChooser wordChooser = new WordChooser();
+        Game game = new Game(wordChooser);
+        System.out.println(game.getWordToGuess());
+
+        do {
+            System.out.println("Enter one letter to guess (" + game.remainingAttempts + " attempts remaining):");
+            
+            Scanner sc= new Scanner(System.in);      
+            Character a = sc.next().charAt(0);
+            game.guessLetter(a); 
+            System.out.println(game.getWordToGuess());
+        
+          }
+          while (game.remainingAttempts > 0);
     }
 }
