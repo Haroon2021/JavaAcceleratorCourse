9efdc145fc4dcb6cb5133ed7d5b802f72cebce80
Makers Admin <MakersAdmin@MacBook-Air.local>
2022-07-25 14:45:48 +0100

    WordChooserTest passing using mock

--- Week1/game/app/src/test/java/game/WordChooserTest.java
+++ Week1/game/app/src/test/java/game/WordChooserTest.java
@@ -3,13 +3,15 @@ package game;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class WordChooserTest {
     @Test
     public void testgetRandomWordFromDictionary() {
-        WordChooser wordchooser = new WordChooser();
-        String randomvar = "MAKERS";
-        assertEquals(wordchooser.getRandomWordFromDictionary(),randomvar);
-    }
-    
+        WordChooser mockedRandom = mock(WordChooser.class);
+        when(mockedRandom.getRandomWordFromDictionary()).thenReturn("MAKERS");
+        assertEquals(String.valueOf("MAKERS"), mockedRandom.getRandomWordFromDictionary());
+    } 
 }
+
